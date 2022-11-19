77c455a8f1f441f2b54542141ba24373155f2988
Dima <dmitriy_evdokimov94@mail.ru>
2022-11-19 11:47:27 +0300

    task_1

---
+++ task/task.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите первое число ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int b = int.Parse(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine(a); 
+    }
+
+else
+{
+    Console.WriteLine(b);
+}
