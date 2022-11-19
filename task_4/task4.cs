f5ef029b163fec56e0daa8e0237278271f88a5c3
Dima <dmitriy_evdokimov94@mail.ru>
2022-11-19 14:11:54 +0300

    task_4

---
+++ task_4/task4.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= n; i += 2)
+{   
+    if (i % 2 == 0);
+     Console.WriteLine(i);
+}    
+
+ 
+ 
+ 
+  
